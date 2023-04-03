2435538f48d1da1d2a8f47b1bf73893356db57a8
Jan Malčák <services@malcak.cz>
2023-04-03 02:25:10 +0200

    Move CORS to responseInterceptor

--- server/index.js
+++ server/index.js
@@ -7,30 +7,22 @@ const proxy = createProxyMiddleware({
     selfHandleResponse: true,
 
     onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-        res.removeHeader('Access-Control-Allow-Origin')
-        return res
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+
+        return responseBuffer
     }),
 
     pathRewrite: { '^/proxy': '' }
 })
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
 async function handler(req, res) {
     return proxy(req, res)
 }
 
-module.exports = allowCors(handler)
+module.exports = handler
